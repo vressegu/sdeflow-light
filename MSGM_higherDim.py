3f2aeecb7ad138501c47272ed578179aa05a3f50
Valentin Resseguier <valentin.resseguier@inrae.fr>
2025-03-07 14:58:41 +0100

    right ordering

--- MSGM_higherDim.py
+++ MSGM_higherDim.py
@@ -245,10 +245,10 @@ if __name__ == '__main__':
                     # sample and plot
                     plt.close('all')
                     for lmbd in lmbds:
+                        x_0 = gen_sde.latent_sample(num_samples, sampler.dim, device=device) # init from prior
                         xs = rk4_stratonovich_sampler(gen_sde, x_0, num_steps, lmbd=lmbd) # sample
 
                         pddatagen = pd.DataFrame(xs[num_steps-1][:,0:dimplot], columns=range(1,1+dimplot))
-                        x_0 = gen_sde.latent_sample(num_samples, sampler.dim, device=device) # init from prior
 
                         fig, axes = plt.subplots(nrows=dimplot, ncols=dimplot, figsize=(2*dimplot,dimplot))
                         color='blue'
