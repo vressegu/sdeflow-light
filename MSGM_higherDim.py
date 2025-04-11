4f7e1f4b24a990b8aa06eae71e42921207474b21
Valentin Resseguier <valentin.resseguier@inrae.fr>
2025-04-11 14:07:33 +0200

    quantitative metric for convergence of the forward SDE

--- MSGM_higherDim.py
+++ MSGM_higherDim.py
@@ -242,7 +242,23 @@ if __name__ == '__main__':
                     # Forward SDE
                     xtest = torch.tensor(xtest, dtype=torch.float32).to(device)
                     for_sde = forward_SDE(inf_sde, T).to(device)
-                    xs_forward = rk4_stratonovich_sampler(for_sde, xtest, num_steps_forward, lmbd=0.,include_t0=True) # sample
+                    xgen_forward = xs_forward[-1]
+                    cov_xtest = torch.cov(xtest.T)
+                    cov_xgen_forward = torch.cov(xgen_forward.T)
+                    xgen_forward_var = torch.var(xgen_forward.T,dim=1)
+                    xgen_forward_var_mean = xgen_forward_var.mean()
+                    print("cov(x_test)")
+                    print(cov_xtest)
+                    print("cov(cov_xgen_forward)")
+                    print(cov_xgen_forward)
+
+                    xtest_var = torch.var(xtest.T,dim=1)
+                    xtest_var_mean = xtest_var.mean()
+                    cov_xgen_forward_converged = xtest_var_mean * torch.eye(sampler.dim).to(device)
+                    # since tr(cov)=E||X||^2 is theoretically conserved
+                    print("cov(cov_xgen_forward) in theory when converged")
+                    print(cov_xgen_forward_converged)
+
                     # indices to visualize
                     fig_step = int(num_steps_forward/10) #100
                     if fig_step < 1:
