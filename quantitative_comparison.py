aa8bffd2449b666638552289b29bfd1d68b27a04
Valentin Resseguier <valentin.resseguier@inrae.fr>
2025-06-30 16:41:51 +0200

    clear memory in quant_comp

--- quantitative_comparison.py
+++ quantitative_comparison.py
@@ -17,6 +17,7 @@ from torchvision import datasets, transforms
 from torchvision.utils import save_image
 import torchvision.utils as vutils
 import torch.backends.cudnn as cudnn
+import gc
 
 def compute_kernel(x, y):
     x_size = x.size(0)
@@ -27,11 +28,16 @@ def compute_kernel(x, y):
     tiled_x = x.expand(x_size, y_size, dim)
     tiled_y = y.expand(x_size, y_size, dim)
     kernel_input = (tiled_x - tiled_y).pow(2).mean(2)/float(dim)
-    return torch.exp(-kernel_input) # (x_size, y_size)
+    result = torch.exp(-kernel_input) # (x_size, y_size)
+    del kernel_input, tiled_x, tiled_y
+    gc.collect()
+    return result # (x_size, y_size)
 
 def compute_mmd(x, y):
     x_kernel = compute_kernel(x, x)
     y_kernel = compute_kernel(y, y)
     xy_kernel = compute_kernel(x, y)
     mmd = x_kernel.mean() + y_kernel.mean() - 2*xy_kernel.mean()
+    del x_kernel, y_kernel, xy_kernel
+    gc.collect()
     return mmd
